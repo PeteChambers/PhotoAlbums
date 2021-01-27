01e9febd113ea22bd5b592293edd53083843846d
Pete Chambers <pcbchambers@gmail.com>
2021-01-27 11:05:46 +0000

    remove pods

--- Pods/Target Support Files/Pods-PhotoAlbums-PhotoAlbumsUITests/Pods-PhotoAlbums-PhotoAlbumsUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoAlbums_PhotoAlbumsUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoAlbums_PhotoAlbumsUITests
-@end
--- Pods/Target Support Files/Pods-PhotoAlbums/Pods-PhotoAlbums-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoAlbums : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoAlbums
-@end
